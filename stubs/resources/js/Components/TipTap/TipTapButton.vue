c70f7dac5d2f21c34325c53e8638e0125b95bb7c
Daniel Cintra <danic10@gmail.com>
2022-12-30 16:29:12 -0300

    add tiptap editor component

---
+++ stubs/resources/js/Components/TipTap/TipTapButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button
+    :title="title"
+    class="m-0 h-6 w-6 cursor-pointer overflow-visible break-words border-none bg-transparent bg-no-repeat p-0 text-center font-sans text-sm normal-case leading-5 text-skin-base-content hover:rounded-sm hover:bg-black hover:text-white"
+  >
+    <i
+      class="text-centertext-sm m-0 cursor-pointer break-words bg-no-repeat p-0 not-italic leading-5"
+      :class="icon"
+    ></i>
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: ''
+  },
+  icon: {
+    type: String,
+    default: ''
+  }
+})
+</script>
---
+++ stubs/resources/js/Components/TipTap/TipTapDivider.vue
@@ -0,0 +1,5 @@
+<template>
+  <div
+    class="my-0 mr-2 ml-2 h-5 w-px break-words bg-skin-base-300 bg-no-repeat p-0 font-sans text-sm leading-5 tracking-normal text-skin-base-content"
+  ></div>
+</template>
