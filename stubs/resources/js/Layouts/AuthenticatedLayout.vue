28b6db1e767e3450f3c06af4bdeb27fb8a03a59d
Daniel Cintra <danic10@gmail.com>
2023-01-31 12:56:31 -0300

    fix title

--- stubs/resources/js/Layouts/AuthenticatedLayout.vue
+++ stubs/resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,43 +1,45 @@
 <template>
-  <AppSideBar
-    :is-side-bar-open="isSideBarOpen"
-    @update-status="toggleSideBar"
-  />
-  <main class="flex flex-1 flex-col" :class="{ 'md:pl-64': isSideBarOpen }">
-    <AppToast />
-    <ConfirmDialog></ConfirmDialog>
-    <AppTopBar @toggle-sidebar="toggleSideBar" />
-    <div class="2xl:mx-16">
-      <slot></slot>
-    </div>
-  </main>
+    <Head title="Modular"></Head>
+    <AppSideBar
+        :is-side-bar-open="isSideBarOpen"
+        @update-status="toggleSideBar"
+    />
+    <main class="flex flex-1 flex-col" :class="{ 'md:pl-64': isSideBarOpen }">
+        <AppToast />
+        <ConfirmDialog></ConfirmDialog>
+        <AppTopBar @toggle-sidebar="toggleSideBar" />
+        <div class="2xl:mx-16">
+            <slot></slot>
+        </div>
+    </main>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { useWindowSize } from '@vueuse/core'
 import { useResizeObserver } from '@vueuse/core'
+import { Head } from '@inertiajs/vue3'
 
 const { width } = useWindowSize()
 
 const isSideBarOpen = ref(true)
 
 if (width.value <= 992) {
-  isSideBarOpen.value = false
+    isSideBarOpen.value = false
 }
 
 useResizeObserver(document.body, (entries) => {
-  const entry = entries[0]
-  const { width } = entry.contentRect
+    const entry = entries[0]
+    const { width } = entry.contentRect
 
-  if (width <= 992) {
-    isSideBarOpen.value = false
-  } else if (width > 992) {
-    isSideBarOpen.value = true
-  }
+    if (width <= 992) {
+        isSideBarOpen.value = false
+    } else if (width > 992) {
+        isSideBarOpen.value = true
+    }
 })
 
 const toggleSideBar = () => {
-  isSideBarOpen.value = !isSideBarOpen.value
+    isSideBarOpen.value = !isSideBarOpen.value
 }
 </script>
--- stubs/resources/js/Layouts/GuestLayout.vue
+++ stubs/resources/js/Layouts/GuestLayout.vue
@@ -1,8 +1,13 @@
 <template>
-  <main>
-    <article>
-      <AppToast />
-      <slot></slot>
-    </article>
-  </main>
+    <Head title="Modular"></Head>
+    <main>
+        <article>
+            <AppToast />
+            <slot></slot>
+        </article>
+    </main>
 </template>
+
+<script setup>
+import { Head } from '@inertiajs/vue3'
+</script>
