65a57bcc02a7ac01fb788e5a01b6418b836a35ee
Daniel Cintra <danic10@gmail.com>
2024-02-19 21:16:52 -0300

    Fix UserSeeder.

--- stubs/modules/User/Database/Seeders/UserSeeder.php
+++ stubs/modules/User/Database/Seeders/UserSeeder.php
@@ -13,7 +13,7 @@ class UserSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
         Schema::disableForeignKeyConstraints();
 
@@ -24,8 +24,6 @@ class UserSeeder extends Seeder
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ]);
-
-        $testUser->assignRole('root');
 
         Schema::enableForeignKeyConstraints();
     }
