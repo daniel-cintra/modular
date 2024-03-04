5b5f38af0b41cb985a334a5e0539497400a38533
Daniel Cintra <danic10@gmail.com>
2024-03-04 11:37:39 -0300

    Add prefix to admin routes.

--- stubs/modules/Dashboard/routes/app.php
+++ stubs/modules/Dashboard/routes/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Dashboard\Http\Controllers\DashboardController;
 
-Route::get('/dashboard', [
-    DashboardController::class, 'index',
-])->name('dashboard.index');
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', [
+        DashboardController::class, 'index',
+    ])->name('dashboard.index');
+});
