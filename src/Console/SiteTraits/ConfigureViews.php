eb70b0f5f9138ce5564291e7b9b64445cd075712
Daniel Cintra <danic10@gmail.com>
2024-03-19 13:20:20 -0300

    Update view config for Laravel 11.

--- src/Console/SiteTraits/ConfigureViews.php
+++ src/Console/SiteTraits/ConfigureViews.php
@@ -8,25 +8,27 @@ trait ConfigureViews
     {
         $configViewFilePath = config_path('view.php');
 
-        if (file_exists($configViewFilePath)) {
-            $this->info('Setting config/view.php...');
-
-            $viewConfig = file_get_contents($configViewFilePath);
-
-            $strSearch = "resource_path('views'),";
-            $strReplace = "resource_path('views'),".PHP_EOL."        base_path('resources-site/views'),";
-
-            $updatedViewConfig = str_replace(
-                $strSearch,
-                $strReplace,
-                $viewConfig
-            );
-
-            file_put_contents($configViewFilePath, $updatedViewConfig);
-
-            $this->info('The config/view.php file was updated.');
-        } else {
-            $this->info('The config/view.php file does not exist.');
+        if (! file_exists($configViewFilePath)) {
+            $this->call('config:publish', [
+                'name' => 'view',
+            ]);
         }
+
+        $this->info('Setting config/view.php...');
+
+        $viewConfig = file_get_contents($configViewFilePath);
+
+        $strSearch = "resource_path('views'),";
+        $strReplace = "resource_path('views'),".PHP_EOL."        base_path('resources-site/views'),";
+
+        $updatedViewConfig = str_replace(
+            $strSearch,
+            $strReplace,
+            $viewConfig
+        );
+
+        file_put_contents($configViewFilePath, $updatedViewConfig);
+
+        $this->info('The config/view.php file was updated.');
     }
 }
