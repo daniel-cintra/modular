a767a4a65b66ce8a78db45eb5d1701a1ffb6f16e
Mehedi Jaman <mail4mjaman@gmail.com>
2024-05-23 07:32:03 +0600

    Feature: Module migration create from stub Feature: created_at and updated_at format with carbon in Model Feature: Name and Created At field show in UI, validate name in request

--- src/Console/MakeMigrationCommand.php
+++ src/Console/MakeMigrationCommand.php
@@ -24,11 +24,11 @@ class MakeMigrationCommand extends Command
         $this->moduleName = Str::studly($this->argument('moduleName'));
         $this->migrationName = $this->argument('migrationName');
 
-        if (! $this->moduleExists()) {
+        if (!$this->moduleExists()) {
             return self::FAILURE;
         }
 
-        $this->comment('Module '.$this->moduleName.' found, creating migration file...');
+        $this->comment('Module ' . $this->moduleName . ' found, creating migration file...');
         $this->createMigrationFile();
 
         return self::SUCCESS;
@@ -38,9 +38,21 @@ class MakeMigrationCommand extends Command
     {
         (new Filesystem)->ensureDirectoryExists(base_path("modules/{$this->moduleName}/Database/Migrations"));
 
-        $this->call('make:migration', [
-            'name' => $this->migrationName,
-            '--path' => "modules/{$this->moduleName}/Database/Migrations",
-        ]);
+        /** Run Laravels default migration command */
+        // $this->call('make:migration', [
+        //     'name' => $this->migrationName,
+        //     '--path' => "modules/{$this->moduleName}/Database/Migrations",
+        // ]);
+
+        /** Create migration according to stub */
+        $stub = file_get_contents(__DIR__ . '/../../stubs/module-stub/modules/Database/Migrations/create_table.stub');
+
+        $tableName = strtolower(Str::plural($this->moduleName));
+
+        $stub = str_replace('{{ tableName }}', $tableName, $stub);
+
+        $path = base_path("modules/{$this->moduleName}/Database/Migrations/" . date('Y_m_d_His_') . "create_{$tableName}_table.php");
+
+        file_put_contents($path, $stub);
     }
 }
