ae7339f31bf395b5d905b86c95993ab442669843
Mehedi Jaman <mail4mjaman@gmail.com>
2024-06-12 20:22:32 +0600

    Fixed typo

--- tests/Console/MakeFactoryCommandTest.php
+++ tests/Console/MakeFactoryCommandTest.php
@@ -17,7 +17,7 @@ it('can run modular:make-factory command', function () {
 it('can generate a factory', function () {
     $this->artisan('modular:make-factory ModuleName FactoryName');
 
-    $factory = base_path('modules/ModuleName/Database/factories/FactoryName.php');
+    $factory = base_path('modules/ModuleName/Database/Factories/FactoryName.php');
     $this->assertTrue(file_exists($factory));
 
     $factoryContent = file_get_contents($factory);
