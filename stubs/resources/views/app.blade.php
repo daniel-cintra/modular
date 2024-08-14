9b98c8127b53b3065546b10731f42e2c85ede301
Daniel Cintra <danic10@gmail.com>
2024-08-14 19:55:09 -0300

    Remove legacy css class.

--- stubs/resources/views/app.blade.php
+++ stubs/resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-skin-base">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
 
 <head>
     <meta charset="utf-8">
