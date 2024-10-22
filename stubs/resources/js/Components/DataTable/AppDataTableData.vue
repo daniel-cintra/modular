15204c17a9bce5ff8118a93a2e1f188a4b785867
Daniel Cintra <danic10@gmail.com>
2024-10-22 13:21:32 -0300

    Updates DataTable vertical padding.

--- stubs/resources/js/Components/DataTable/AppDataTableData.vue
+++ stubs/resources/js/Components/DataTable/AppDataTableData.vue
@@ -1,6 +1,6 @@
 <template>
     <td
-        class="whitespace-nowrap border border-skin-neutral-6 px-2 py-1 font-medium"
+        class="whitespace-nowrap border border-skin-neutral-6 px-2 py-1.5 font-medium"
     >
         <slot></slot>
     </td>
